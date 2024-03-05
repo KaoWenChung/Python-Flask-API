473b48ebf4ce9cfeeb940325242ec1a6a1521b47
wyn <k022298@gmail.com>
2024-03-05 00:10:13 +0000

    Added a index.html file to integrate with python

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Owen'}
+    posts = [
+        {
+            'author': {'username': 'Mike'},
+            'body': 'Beautiful day in the UK!'
+        },
+        {
+            'author': {'username': 'Amy'},
+            'body': 'Having a dating with Connor'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
